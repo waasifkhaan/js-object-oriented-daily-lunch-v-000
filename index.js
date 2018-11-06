6ee79c9694714d67ab476618a975b6fe2e7775d8
Wasif Khan <waasif.khaan@gmail.com>
2018-11-06 21:41:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,70 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0;
+
+class Neighborhood {
+    constructor(name) {
+        this.id = ++neighborhoodId;
+        this.name = name;
+
+        store.neighborhoods.push(this);
+      }
+      deliveries () {
+      return store.deliveries.filter(delivery => {
+        return delivery.neighborhoodId === this.id;}
+      )};
+      customers () {
+        return store.customers.filter(customer =>{
+          return customer.neighborhoodId=this.id;
+      }
+    )
+
+}
+
+
+  }
+  let customerId = 0;
+
+  class Customer {
+      constructor(name,neighborhood) {
+          this.id = ++customerId;
+          this.neighborhoodId = neighborhood
+          this.name = name;
+
+          store.customers.push(this);
+        }
+
+    }
+
+    let mealId = 0;
+
+    class Meal {
+        constructor(title,price) {
+            this.id = ++mealId;
+            this.title = title;
+            this.price = price;
+
+            store.meals.push(this);
+          }
+
+      }
+      let deliveryId = 0;
+
+      class Delivery {
+          constructor(customer,neighborhood,meal) {
+              this.id = ++deliveryId;
+              this.mealId = meal;
+              this.neighborhoodId = neighborhood;
+              this.customerId =customer
+
+              store.deliveries.push(this);
+            }
+      
+        customer() {
+          return store.customers.find(customer => {
+            return customer.id === this.customerId;
+    });
+  }
+
+      }
