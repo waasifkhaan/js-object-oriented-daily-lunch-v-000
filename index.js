a1d91196dcdf451eee9e0b9a3d1b366ed4464b03
Wasif Khan <waasif.khaan@gmail.com>
2018-11-06 23:32:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,9 +70,9 @@ class Neighborhood {
             const allCustomers = this.deliveries().map(delivery => delivery.customer());
               return [...new Set(allCustomers)];
             }
-
+            
           static byPrice() {
-            return store.meals.sort(function (a, b) => {return a.price > b.price});
+            return store.meals.sort((a,b) a.price > b.price);
           }
 
       }
